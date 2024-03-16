77814132252e488b87a19e643a1ebffe6340ee81
“박현진” <dev.hyunjin.park@gmail.com>
2024-03-16 15:39:16 +0900

    10_particle text

---
+++ 10_particle_text/script.js
@@ -0,0 +1,192 @@
+window.addEventListener("load", () => {
+  const canvas = document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d", {
+    willReadFrequently: true, // 성능 최적화
+  });
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  class Particle {
+    constructor(effect, x, y, color) {
+      this.effect = effect;
+      this.x = Math.random() * this.effect.canvasWidth;
+      this.y = this.effect.canvasHeight;
+      this.color = color;
+      this.originX = x;
+      this.originY = y;
+      this.size = this.effect.gap; // effect와 같은 해상도
+      this.dx = 0;
+      this.dy = 0;
+      this.vx = 0;
+      this.vy = 0;
+      this.force = 0;
+      this.angle = 0;
+      this.distance = 0;
+      this.friction = Math.random() * 0.6 + 0.15;
+      this.ease = Math.random() * 0.1 + 0.005;
+    }
+    draw() {
+      this.effect.context.fillStyle = this.color;
+      this.effect.context.fillRect(this.x, this.y, this.size, this.size);
+    }
+    update() {
+      this.dx = this.effect.mouse.x - this.x;
+      this.dy = this.effect.mouse.y - this.y;
+      this.distance = this.dx * this.dx + this.dy * this.dy;
+      // Math.hypot(dy, dx)
+      this.force = -this.effect.mouse.radius / this.distance;
+
+      if (this.distance < this.effect.mouse.radius) {
+        this.angle = Math.atan2(this.dy, this.dx);
+        this.vx += this.force * Math.cos(this.angle);
+        this.vy += this.force * Math.cos(this.angle);
+      }
+
+      this.x +=
+        (this.vx *= this.friction) + (this.originX - this.x) * this.ease;
+      this.y +=
+        (this.vy *= this.friction) + (this.originY - this.y) * this.ease;
+    }
+  }
+
+  class Effect {
+    constructor(context, canvasWidth, canvasHeight) {
+      this.context = context;
+      this.canvasWidth = canvasWidth;
+      this.canvasHeight = canvasHeight;
+      this.textX = this.canvasWidth / 2;
+      this.textY = this.canvasHeight / 2;
+      this.fontSize = 80;
+      this.lineHeight = this.fontSize * 1.9;
+      this.maxTextWidth = this.canvasWidth * 0.8;
+      this.textInput = document.getElementById("textInput");
+      // this.verticalOffset = -160;
+      this.textInput.addEventListener("keyup", (e) => {
+        if (e.key !== " ") {
+          this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+          this.wrapText(e.target.value);
+        }
+      });
+      // particle text
+      this.particles = [];
+      this.gap = 3; // 해상도 3x3보다 작아지면 퍼포먼스 이슈 있을 수 있음
+      this.mouse = {
+        radius: 2000,
+        x: 0,
+        y: 0,
+      };
+      window.addEventListener("mousemove", (e) => {
+        this.mouse.x = e.x;
+        this.mouse.y = e.y;
+      });
+    }
+
+    wrapText(text) {
+      const gradient = this.context.createLinearGradient(
+        0,
+        0,
+        canvas.width,
+        canvas.height
+      );
+      gradient.addColorStop(0.3, "yellow");
+      gradient.addColorStop(0.5, "pink");
+      gradient.addColorStop(0.7, "blue");
+
+      this.context.strokeStyle = "white";
+      this.context.fillStyle = gradient;
+      this.context.font = `${this.fontSize}px Helvetica`;
+      this.context.textAlign = "center";
+      this.context.textBaseline = "middle";
+      this.context.lineWidth = 3;
+
+      // break multiline text
+      let linesArray = [];
+      let words = text.split(" ");
+      let lineCounter = 0;
+      let line = "";
+      for (let i = 0; i < words.length; i++) {
+        let testLine = line + words[i] + " ";
+        if (this.context.measureText(testLine).width > this.maxTextWidth) {
+          line = words[i] + " ";
+          lineCounter++;
+        } else {
+          line = testLine;
+        }
+        linesArray[lineCounter] = line;
+      }
+      let textHeight = this.lineHeight * lineCounter;
+      this.textY = this.canvasHeight / 2 - textHeight / 2;
+
+      linesArray.forEach((el, index) => {
+        this.context.fillText(
+          el,
+          this.textX,
+          this.textY + index * this.lineHeight
+        );
+        this.context.strokeText(
+          el,
+          this.textX,
+          this.textY + index * this.lineHeight
+        );
+      });
+      this.convertParticles();
+    }
+
+    convertParticles() {
+      this.particles = [];
+      const pixels = this.context.getImageData(
+        0,
+        0,
+        this.canvasWidth,
+        this.canvasHeight
+      ).data;
+
+      this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+      for (let y = 0; y < this.canvasHeight; y += this.gap) {
+        for (let x = 0; x < this.canvasWidth; x += this.gap) {
+          const index = (y * this.canvasWidth + x) * 4;
+          const alpha = pixels[index + 3];
+          if (alpha > 0) {
+            const red = pixels[index];
+            const green = pixels[index + 1];
+            const blue = pixels[index + 2];
+            const color = `rgb(${red}, ${green}, ${blue})`;
+            this.particles.push(new Particle(this, x, y, color));
+          }
+        }
+      }
+    }
+    render() {
+      this.particles.forEach((particle) => {
+        particle.update();
+        particle.draw();
+      });
+    }
+
+    resize(width, height) {
+      this.canvasWidth = width;
+      this.canvasHeight = height;
+      this.textX = this.canvasWidth / 2;
+      this.textY = this.canvasHeight / 2;
+      this.maxTextWidth = this.canvasWidth * 0.8;
+    }
+  }
+
+  const effect = new Effect(ctx, canvas.width, canvas.height);
+  effect.wrapText(effect.textInput.value);
+  effect.render();
+
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    effect.render();
+    requestAnimationFrame(animate);
+  }
+  animate();
+
+  window.addEventListener("resize", () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    effect.resize(canvas.width, canvas.height);
+    effect.wrapText(effect.textInput.value);
+  });
+});
