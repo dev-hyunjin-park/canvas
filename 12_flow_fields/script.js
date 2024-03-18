8ff4de1692b1646af36e7e37a10245cd5926eefe
“박현진” <dev.hyunjin.park@gmail.com>
2024-03-18 21:04:04 +0900

    12_flow_fields - random lines with image pattern

---
+++ 12_flow_fields/script.js
@@ -0,0 +1,88 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+canvas.width = 700;
+canvas.height = 900;
+
+// global settings
+ctx.lineWidth = 10;
+
+// canvas shadows
+// ctx.shadowOffsetX = 2;
+// ctx.shadowOffsetY = 2;
+// ctx.shadowColor = "black";
+
+// canvas pattern
+const patternImage = document.getElementById("patternImage");
+const pattern1 = ctx.createPattern(patternImage, "no-repeat");
+ctx.strokeStyle = pattern1;
+
+class Line {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.x = Math.random() * this.canvas.width;
+    this.y = Math.random() * this.canvas.height;
+    this.history = [{ x: this.x, y: this.y }];
+    // 하위 픽셀 값을 얻지 않도록 Math.floor
+    this.hue = Math.floor(Math.random() * 360);
+    this.lineWidth = Math.floor(Math.random() * 15 + 10);
+    this.maxLength = Math.floor(Math.random() * 10 + 10);
+    this.speedX = Math.random() * 1 - 0.5;
+    this.speedY = 5;
+    this.lifeSpan = this.maxLength * 10;
+    this.timer = 0;
+  }
+  draw(context) {
+    // hsl(색조, 채도, 밝기)
+    // context.strokeStyle = `hsl(${this.hue}, 100%, 50%)`;
+    context.lineWidth = this.lineWidth;
+    context.beginPath();
+    context.moveTo(this.history[0].x, this.history[0].y);
+    for (let i = 0; i < this.history.length; i++) {
+      context.lineTo(this.history[i].x, this.history[i].y);
+    }
+    context.stroke();
+  }
+
+  update() {
+    this.timer++;
+    if (this.timer < this.lifeSpan) {
+      this.x += this.speedX + Math.random() * 20 - 10;
+      this.y += this.speedY + Math.random() * 20 - 10;
+      // this.x = Math.random() * this.canvas.width;
+      // this.y = Math.random() * this.canvas.height;
+      this.history.push({ x: this.x, y: this.y });
+
+      if (this.history.length > this.maxLength) {
+        this.history.shift(); // remove the first element
+      }
+    } else if (this.history.length <= 1) {
+      this.reset();
+    } else {
+      this.history.shift();
+    }
+  }
+  reset() {
+    this.x = Math.random() * this.canvas.width;
+    this.y = Math.random() * this.canvas.height;
+    this.history = [{ x: this.x, y: this.y }];
+    this.timer = 0;
+  }
+}
+
+const linesArray = [];
+const numberOfLines = 500;
+for (let i = 0; i < numberOfLines; i++) {
+  linesArray.push(new Line(canvas));
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  linesArray.forEach((line) => {
+    line.draw(ctx);
+    line.update(ctx);
+  });
+
+  requestAnimationFrame(animate);
+}
+
+animate();
